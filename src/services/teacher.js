cc9fa92fff9a034747d5dfc85fd73ecb61e1b5ed
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2025-12-12 19:59:43 +0800

    getStudentsService, getStudentService hiiv

--- src/services/teacher.js
+++ src/services/teacher.js
@@ -1,12 +1,27 @@
 import { db } from "../db.js";
 
-export async function getStudentsService(teacherId, search) {
+export const getStudentsService = async (student) => {
+  const response = await db.query(
+    `SELECT * FROM users users WHERE role = ${student}`
+  );
   // TODO: SELECT id, firstname, lastname, age FROM users WHERE teacher_id = $1 AND role = 'student'
   // TODO: search query байвал нэрээр нь ILIKE ашиглаад шүүх
-  return [];
-}
+  return response.rows;
+};
 
-export async function createStudentService(teacherId, firstname, lastname, age, password) {
+export const getStudentService = async (student, id) => {
+  const response = await db.query(
+    `SELECT * FROM users WHERE role = ${student} and id=${id}`
+  );
+  return response.rows[0];
+};
+export async function createStudentService(
+  teacherId,
+  firstname,
+  lastname,
+  age,
+  password
+) {
   // TODO: INSERT INTO users (firstname, lastname, age, password, role, teacher_id) VALUES ($1, $2, $3, $4, 'student', $5) RETURNING id
   return { ok: false, todo: true };
 }
