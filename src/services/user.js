06c7b905afa5e186a5f49ddf2a0377b0d5ed94b4
vmbvv <chingun.0829@gmail.com>
2025-12-12 19:04:20 +0800

    registerTeacherService query

--- src/services/user.js
+++ src/services/user.js
@@ -1,12 +1,22 @@
 import { db } from "../db.js";
 
-export async function registerTeacherService(firstname, lastname, age, password) {
+export async function registerTeacherService(
+  firstname,
+  lastname,
+  age,
+  password
+) {
   // TODO: INSERT INTO users (firstname, lastname, age, password, role) VALUES ($1, $2, $3, $4, 'teacher') RETURNING id
-  return { ok: false, todo: true };
+  const response = await db.query(
+    `INSERT INTO users (firstname, lastname, age, password, role) VALUES ($1, $2, $3, $4, 'teacher' RETRUNING id`,
+    [firstname, lastname, age, password]
+  );
+  return response.rows[0];
 }
 
 export async function loginTeacherService(id, password) {
   // TODO: багшийн id, нууц үгийг users хүснэгтээс шалгах
+
   // TODO: амжилттай бол cookie-нд хадгалах teacher_id утгыг backend-ээс буцаах
   return { ok: false, todo: true };
 }
